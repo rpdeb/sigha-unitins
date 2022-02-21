40bb731f918406819c91ba3e013fb4762113c29b
Debora Ribeiro Pereira <52083218+rpdeb@users.noreply.github.com>
2022-02-21 01:13:18 -0300

    Update Estrutura

--- sigha-unitins/src/router/index.js
+++ sigha-unitins/src/router/index.js
@@ -7,8 +7,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Login',
+    component: () => import('../views/Login.vue')
   },
   {
     path: '/about',
