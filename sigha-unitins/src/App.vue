67868d55c35411d16b7fd0b7580a2f32cbcac2ae
Anthony Silveira <anthonysilveira102@gmail.com>
2022-02-21 11:51:02 -0300

    Commit Ajuste de Codigo

--- sigha-unitins/src/App.vue
+++ sigha-unitins/src/App.vue
@@ -51,12 +51,6 @@
       </tbody>
     </table>
   </div>
-  <!-- <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-    <div> -->
 </template>
 
 <style>
